a7a62cd53beddb9af2b029fc0741f69fb3e17149
Fred <robert.frederic7@gmail.com>
2024-08-02 14:26:17 +0200

    3.0.4

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Modal = void 0;
+exports["default"] = Modal;
 var _react = _interopRequireDefault(require("react"));
 var _propTypes = _interopRequireDefault(require("prop-types"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
@@ -13,25 +13,48 @@ Modal.propTypes = {
   modalMessage: _propTypes["default"].string,
   color: _propTypes["default"].string
 };
-var Modal = exports.Modal = function Modal(_ref) {
+function Modal(_ref) {
   var onClose = _ref.onClose,
-    errorMessage = _ref.errorMessage,
     modalMessage = _ref.modalMessage,
     _ref$color = _ref.color,
     color = _ref$color === void 0 ? 'blue' : _ref$color;
   var modalStyle = {
-    backgroundColor: color,
     position: "absolute",
     top: "50px",
-    left: "calc(50% - 150px)",
-    width: "300px",
+    left: "calc(50% - min(50%, 150px))",
+    width: "50%",
+    maxWidth: "300px",
+    minWidth: "min(100%, 300px)",
     minHeight: "100px",
-    border: "1px solid black",
     borderRadius: "5px",
     padding: "10px"
   };
+  var buttonStyle = {
+    marginLeft: "auto",
+    marginTop: "-20px",
+    marginRight: "-20px"
+  };
+  var overlayStyle = {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "rgba(0,0,0,0.7)",
+    height: "100%",
+    width: "100%"
+  };
   return /*#__PURE__*/_react["default"].createElement("div", {
-    className: "modal-react-p14",
+    style: overlayStyle
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "modal-react-p14 rounded-md p-4",
     style: modalStyle
-  }, /*#__PURE__*/_react["default"].createElement("h1", null, "Modal"));
-};
+  }, /*#__PURE__*/_react["default"].createElement(Image, {
+    src: "/img/cross.svg",
+    onClick: onClose,
+    style: buttonStyle
+  }), /*#__PURE__*/_react["default"].createElement("div", {
+    className: "mt-2 text-sm font-medium"
+  }, /*#__PURE__*/_react["default"].createElement("p", null, modalMessage))));
+}
+;
--- src/index.js
+++ src/index.js
@@ -8,23 +8,52 @@ Modal.propTypes = {
     color: PropTypes.string,
 };
 
-export default function Modal({ onClose, errorMessage, modalMessage, color = 'blue' }) {
+export default function Modal({ onClose, modalMessage, color = 'blue' }) {
 
     const modalStyle = {
-        backgroundColor: color,
         position: "absolute",
         top: "50px",
-        left: "calc(50% - 150px)",
-        width: "300px",
+        left: "calc(50% - min(50%, 150px))",
+        width: "50%",
+        maxWidth: "300px",
+        minWidth: "min(100%, 300px)",
         minHeight: "100px",
-        border: "1px solid black",
         borderRadius: "5px",
         padding: "10px",
     };
 
+    const buttonStyle = {
+        marginLeft: "auto",
+        marginTop: "-20px",
+        marginRight: "-20px"
+    };
+
+    let overlayStyle = {
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: "rgba(0,0,0,0.7)",
+        height: "100%",
+        width: "100%",
+    };
     return (
-        <div className="modal-react-p14" style={modalStyle}>
-            <h1>Modal</h1>
+        <div style={overlayStyle}>
+            <div className="modal-react-p14 rounded-md p-4" style={modalStyle}>
+                <Image src="/img/cross.svg" onClick={onClose} style={buttonStyle}/>
+                <div className="mt-2 text-sm font-medium">
+                    <p>{modalMessage}</p>
+                    {/*<button*/}
+                    {/*    type="button"*/}
+                    {/*    style={buttonStyle}*/}
+                    {/*    onClick={onClose}*/}
+                    {/*    className={"modal-react-p14-button"}*/}
+                    {/*>*/}
+                    {/*    Dismiss*/}
+                    {/*</button>*/}
+                </div>
+            </div>
         </div>
     );
 };
