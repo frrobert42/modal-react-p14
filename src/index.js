5330563fb91637cdb70de7479ec715721080d6d4
Fred <robert.frederic7@gmail.com>
2024-08-02 14:41:22 +0200

    3.1.3

--- src/index.js
+++ src/index.js
@@ -26,7 +26,6 @@ export default function Modal({ onClose, modalMessage, color = 'blue' }) {
         marginLeft: "auto",
         marginTop: "-20px",
         marginRight: "-20px",
-        backgroundColor: "white",
     };
 
     let overlayStyle = {
@@ -46,7 +45,7 @@ export default function Modal({ onClose, modalMessage, color = 'blue' }) {
                     style={buttonStyle}
                     onClick={onClose}
                     width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <rect x="0" fill="none" width="24" height="24"/>
+                    <rect x="0" fill="none" width="24" height="24" style={{backgroundColor: "white"}}/>
                     <g>
                         <path d="M19.1 4.9C15.2 1 8.8 1 4.9 4.9S1 15.2 4.9 19.1s10.2 3.9 14.1 0 4-10.3.1-14.2zm-4.3 11.3L12 13.4l-2.8 2.8-1.4-1.4 2.8-2.8-2.8-2.8 1.4-1.4 2.8 2.8 2.8-2.8 1.4 1.4-2.8 2.8 2.8 2.8-1.4 1.4z"/>
                     </g>
