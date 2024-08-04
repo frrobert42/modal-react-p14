3aaf16782fe46bdf41350aa0e8180c08feb48355
Fred <robert.frederic7@gmail.com>
2024-08-04 14:07:19 +0200

    4.0.1 fix build

--- dist/index.js
+++ dist/index.js
@@ -9,15 +9,12 @@ var _propTypes = _interopRequireDefault(require("prop-types"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
 Modal.propTypes = {
   onClose: _propTypes["default"].func,
-  errorMessage: _propTypes["default"].string,
-  modalMessage: _propTypes["default"].string,
-  color: _propTypes["default"].string
+  modalMessage: _propTypes["default"].string
 };
 function Modal(_ref) {
   var onClose = _ref.onClose,
-    modalMessage = _ref.modalMessage,
-    _ref$color = _ref.color,
-    color = _ref$color === void 0 ? 'blue' : _ref$color;
+    modalMessage = _ref.modalMessage;
+  // Styles
   var modalStyle = {
     position: "absolute",
     top: "50px",
@@ -60,11 +57,7 @@ function Modal(_ref) {
     fill: "none",
     width: "24",
     height: "24"
-  }), /*#__PURE__*/_react["default"].createElement("g", {
-    style: {
-      backgroundColor: "white"
-    }
-  }, /*#__PURE__*/_react["default"].createElement("path", {
+  }), /*#__PURE__*/_react["default"].createElement("g", null, /*#__PURE__*/_react["default"].createElement("path", {
     d: "M19.1 4.9C15.2 1 8.8 1 4.9 4.9S1 15.2 4.9 19.1s10.2 3.9 14.1 0 4-10.3.1-14.2zm-4.3 11.3L12 13.4l-2.8 2.8-1.4-1.4 2.8-2.8-2.8-2.8 1.4-1.4 2.8 2.8 2.8-2.8 1.4 1.4-2.8 2.8 2.8 2.8-1.4 1.4z"
   }))), /*#__PURE__*/_react["default"].createElement("div", {
     className: "mt-2 text-sm font-medium"
